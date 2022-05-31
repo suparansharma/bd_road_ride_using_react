313c14e90cb75f03b2e5159a16b5aad45988b96c
suparansharma <suparansharmap@gmail.com>
2022-06-01 00:43:55 +0600

    home page design done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Home.css';
+import bike from '../../images/Bike.png'
+import bus from '../../images/Bus.png'
+import train from '../../images/Train.png'
+import car from '../../images/Car.png'
 
 const Home = () => {
     return (
         <div>
     <div class="row rowOfhome container">
-  <div class=" col-md-3 singleColum">.col-6 .col-md-4</div>
-  <div class=" col-md-3 singleColum">.col-6 .col-md-4</div>
-  <div class=" col-md-3 singleColum">.col-6 .col-md-4</div>
-  <div class=" col-md-3 singleColum">.col-6 .col-md-4</div>
+  <div class=" col-md-3 singleColum">
+      <img src={bike} alt="" srcset="" />
+  </div>
+  <div class=" col-md-3 singleColum">
+      <img src={car} alt="" srcset="" />
+      </div>
+  <div class=" col-md-3 singleColum">
+  <img src={bus} alt="" srcset="" />
+      </div>
+  <div class=" col-md-3 singleColum">
+  <img src={train} alt="" srcset="" />
+      </div>
 </div>
         </div>
     );
