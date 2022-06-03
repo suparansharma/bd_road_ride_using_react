484b14567f1b1402812424240dcedfa79683af4b
suparansharma <suparansharmap@gmail.com>
2022-06-04 00:56:56 +0600

    login pages

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,6 +6,12 @@ const Login = () => {
              
             <div className="registrationArea">
             <h2>Create An Account</h2>
+            <form action="">
+                <input type="text" name="name" id="" placeholder='Enter Your Name' required /><br />
+                <input type="text" name="email" id="" placeholder='Enter Your Email' required /><br />
+                <input type="password" name="password" id="" placeholder='Enter Your Password' required /><br />
+                <input type="submit" value="Submit" />
+            </form>
             
 
             </div>
